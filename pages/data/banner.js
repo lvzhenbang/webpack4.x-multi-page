0ce7e2353d0585f8291674ecb89f2fe5b779f403
lvzhenbang <lvzhenbang@outlook.com>
2019-01-10 21:48:38 +0800

    fix: banner

---
+++ pages/data/banner.js
@@ -0,0 +1,19 @@
+const loadImg = require('@utils/load-img')
+
+module.exports = [
+  {
+    href: './styleus.html',
+    img: loadImg('index/banner-1.jpg'),
+    title: '企业风采'
+  },
+  {
+    href: 'jvascrtip:;',
+    img: loadImg('index/banner-2.jpg'),
+    title: ''
+  },
+  {
+    href: 'javascript:;',
+    img: loadImg('index/banner-3.jpg'),
+    title: ''
+  }
+]
