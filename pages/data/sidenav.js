f234a4e1005e56a59fb66f7bd5b370a59d00c8e8
lvzhenbang <lvzhenbang@outlook.com>
2019-01-21 01:31:19 +0800

    fix: sidenav data

---
+++ pages/data/sidenav.js
@@ -0,0 +1,102 @@
+module.exports = {
+  zjsj: {
+    cn_name: '走进圣捷',
+    en_name: 'INTO SHENGJIE',
+    tabs: [
+      {
+        cn_name: '圣捷投资',
+        en_name: 'SHENGJIE INVESTMENT',
+        type: 'investment'
+      }, {
+        cn_name: '董事长致辞',
+        en_name: 'CHAIRMANS SPEECH',
+        type: 'speech'
+      }, {
+        cn_name: '企业文化',
+        en_name: 'CORPORATE CULTURE',
+        type: 'culture'
+      }, {
+        cn_name: '圣捷大事记',
+        en_name: 'SHENGJIE BIG EVENTT',
+        type: 'event'
+      }, {
+        cn_name: '发展战略',
+        en_name: 'DEVELOPMENT STRATEGY',
+        type: 'strategy'
+      }, {
+        cn_name: '管理团队',
+        en_name: 'MANAGEMENT TEAM',
+        type: 'team'
+      }
+    ]
+  },
+  ywbk: {
+    cn_name: '业务板块',
+    en_name: 'BUSINESS SECTOR',
+    tabs: [
+      {
+        cn_name: '互联网金融',
+        en_name: 'ONLINE FINANCE',
+        type: 'finance'
+      }, {
+        cn_name: '全品类金融',
+        en_name: 'WHOLE CATEGORY FINANCE',
+        type: 'allfinance'
+      }, {
+        cn_name: '商业地产',
+        en_name: 'COMMERCIAL REAL ESTATE',
+        type: 'estate'
+      }, {
+        cn_name: '餐饮',
+        en_name: 'RESTAURANT',
+        type: 'restaurant'
+      }, {
+        cn_name: '文化',
+        en_name: 'CULTURE',
+        type: 'culture2'
+      }, {
+        cn_name: '大健康',
+        en_name: 'BIG HEALTH',
+        type: 'health'
+      }
+    ]
+  },
+  shzr: {
+    cn_name: '社会责任',
+    en_name: 'SOCIAL RESPONSIBILITY',
+    tabs: [
+      {
+        cn_name: '责任体系',
+        en_name: 'RESPONSIBILITY SYSTEM',
+        type: 'system'
+      }, {
+        cn_name: '社会公益',
+        en_name: 'SOCIAL WELFARE',
+        type: 'welfare'
+      }, {
+        cn_name: '光彩事业',
+        en_name: 'GLORIOUS CAREER',
+        type: 'career'
+      }
+    ]
+  },
+  sxy: {
+    cn_name: '圣捷商学院',
+    en_name: 'SHENGJIE BUSINESS SCHOOL',
+    tabs: [
+      {
+        cn_name: '圣捷商学院',
+        en_name: 'SHENGJIE BUSINESS SCHOOL',
+        type: 'school'
+      }, {
+        cn_name: '商学院名人',
+        en_name: 'BUSINESS SCHOOL CELEBRITIES',
+        type: 'celebrites'
+      }, {
+        cn_name: '商学院课堂',
+        en_name: 'BUSINESS SCHOOL CLASSROOM',
+        type: 'classroom'
+      }
+    ]
+  }
+}
