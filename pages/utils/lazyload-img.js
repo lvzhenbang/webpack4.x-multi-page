07eb5afcd078a4373ccf63cc2ae6b8e2564d9eec
lvzhenbang <lvzhenbang@outlook.com>
2019-04-20 10:41:04 +0800

    fix: lazyload

--- pages/utils/lazyload-img.js
+++ pages/utils/lazyload-img.js
@@ -1,49 +1,41 @@
+require('intersection-observer');
+
 class Lazyload {
   constructor(imgs) {
     this.imgs = imgs
-    this.observer = null
-  }
-  
-  intersection() {
-    let self = this
-    if ('IntersectionObserver' in window) {
-      this.observer = new IntersectionObserver(function(entries){
-        entries.forEach(function(entry) {
-          if(entry.intersectionRatio > 0) {
-            self.observer.unobserve(entry.target)
-
-            var datasrc = entry.target.getAttribute('data-src')
-            var datasrcset = entry.target.getAttribute('data-srcset')
-  
-            if (entry.target.tagName.toLowerCase() === 'img') {
-              if (datasrc) {
-                entry.target.src = datasrc
-              }
-  
-              if (datasrcset) {
-                entry.target.srcset = datasrcset
-              }
-            } else {
-              entry.target.getElementsByClassName.backgroudImage = 'url(' + datasrc + ')'
-            }
-          }
-        })
-      }, {
-        root: null,
-        rootMargin: "0px",
-        threshold: [0]
-      })
-      
-      this.imgs.forEach(function (img) {
-        self.observer.observe(img);
-      })
-    } else {
-      console.log("IntersectionObserver isn't support.")
-    }
   }
   
   init() {
-    this.intersection()
+    let observer = new IntersectionObserver(function(entries){
+      entries.forEach(function(entry) {
+        if(entry.intersectionRatio > 0) {
+          observer.unobserve(entry.target)
+
+          var datasrc = entry.target.getAttribute('data-src')
+          var datasrcset = entry.target.getAttribute('data-srcset')
+
+          if (entry.target.tagName.toLowerCase() === 'img') {
+            if (datasrc) {
+              entry.target.src = datasrc
+            }
+
+            if (datasrcset) {
+              entry.target.srcset = datasrcset
+            }
+          } else {
+            entry.target.getElementsByClassName.backgroudImage = 'url(' + datasrc + ')'
+          }
+        }
+      })
+    }, {
+      root: null,
+      rootMargin: "0px",
+      threshold: [0]
+    })
+    
+    this.imgs.forEach(function (img) {
+      observer.observe(img);
+    })
   }
 }
 
