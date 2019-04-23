33fd9f811d1990a92ede806dc32c4f50728d535f
lvzhenbang <lvzhenbang@outlook.com>
2019-04-23 16:05:59 +0800

    fix: deploy-url

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   host: {
     devUrl: 'http://localhost:8080/', // 开发地址
-    deployUrl: 'http://localhost:8080/' // 发布地址
+    deployUrl: 'https://lvzhenbang.github.io/webpack4.x-multi-page/dist/' // 发布地址
   },
   sprites: [ // 准备做雪碧图处理的图片所在目录名称
     'aboutus'
